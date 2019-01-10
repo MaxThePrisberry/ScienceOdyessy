2554803413fd1c242c5ec758f60ff2cd218eae51
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2019-01-10 08:39:59 -0800

    Update BasicCipher.java

--- BasicCipher.java
+++ BasicCipher.java
@@ -1,12 +1,15 @@
 package caesarCiphers;
 
+//imports the scanner we use to take input from the console
 import java.util.Scanner;
 
 public class BasicCipher {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
+		//this boolean controls whether the basic code should loop and run again
 		boolean going = true;
 		while (going){
+			//this 'correct' boolean and the while loop run until a valid answer is given
 			boolean correct = false;
 			String result = null;
 			while (!correct){
@@ -18,11 +21,13 @@ public class BasicCipher {
 					System.out.println("Your input does not meet either answer. Please try again.");
 				}
 			}
+			//gets the answer, and determines if encoding or decoding runs
 			if (result.equals("encoding")){
 				encoding();
 			} else if (result.equals("decoding")){
 				decoding();
 			}
+			//once everything is done, asks if the program should loop
 			System.out.println("Would you like to run this program again? (Y/N)");
 			result = scan.next();
 			if (!result.equals("Y")){going = false;}
@@ -30,6 +35,7 @@ public class BasicCipher {
 	}
 	
 	private static void encoding(){
+		//ask for the message and the shift index
 		Scanner scan = new Scanner(System.in);
 		String output = "";
 		System.out.println("What message are you going to be encoding?");
@@ -37,6 +43,7 @@ public class BasicCipher {
 		message = message.toLowerCase();
 		System.out.println("What index would you like to shift the message by?");
 		int index = scan.nextInt();
+		//shifts everything by the index, unless it is a space, then print space
 		for (int x = 0; x < message.length(); x++){
 			if (message.charAt(x) != 32){
 				output += (char)(97 + ((message.charAt(x) + index - 97) % 26));
@@ -44,10 +51,12 @@ public class BasicCipher {
 				output += " ";
 			}
 		}
+		//ouptus the message to the console
 		System.out.println("Your message is " + output + ".");
 	}
 	
 	private static void decoding(){
+		//asks for the message and the shift index
 		Scanner scan = new Scanner(System.in);
 		String output = "";
 		System.out.println("What message are you going to be decoding?");
@@ -55,6 +64,7 @@ public class BasicCipher {
 		message = message.toLowerCase();
 		System.out.println("What index is the message shifted by?");
 		int index = scan.nextInt();
+		//shifts everything back by the index, unless it is a space, then print space
 		for (int x = 0; x < message.length(); x++){
 			if (message.charAt(x) != 32){
 				output += (char)(97 + ((26 + (message.charAt(x) - 97 - index)) % 26));
@@ -62,6 +72,7 @@ public class BasicCipher {
 				output += " ";
 			}
 		}
+		//outputs the message to the console
 		System.out.println("Your message is " + output + ".\n");
 	}
 }
