03f2a07c39aa41f2e367e03478f3b980ece82ed7
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2019-01-10 16:04:18 -0800

    Update BruteForceCipher.java

--- BruteForceCipher.java
+++ BruteForceCipher.java
@@ -1,35 +1,85 @@
+package caesarCiphers;
+
 import java.util.*; 
 import java.io.*;
+
 public class BruteForceCipher {
 	public static void main(String [] args) throws FileNotFoundException{
 		Scanner scan = new Scanner(System.in);   
 		System.out.print("Enter an encrypted word to be decoded: ");
-		String str = scan.next(); 
-		BruteForce(str); 
+		String str = scan.nextLine(); 
+		str.trim();
+		BruteForce(str);
 	}
-	public static void BruteForce(String str){
+	public static void BruteForce(String str) throws FileNotFoundException{
 		ArrayList<String> results = new ArrayList<>(); 
-		String message = "";
-		for(int i = 1; i < 26; i++){
+		for(int i = 1; i <= 26; i++){
+			String message = "";
 			for(int j = 0; j < str.length(); j++){
 				if (str.charAt(j) != 32){
 					message += (char)(97 + ((26 + (str.charAt(j) - 97 - i)) % 26));
 				}else{
 					message += " ";
 				}
-				results.add(message); 
-				System.out.println("Result " + i + ": " + message);
-				message = ""; 
+			}
+			results.add(message); 
+			System.out.println("Shift " + i + ": " + message);
+		}
+		bestGuess(results);
+	}
+	public static void bestGuess(ArrayList<String> results) throws FileNotFoundException{
+		double[] wordMatch = new double[26];
+		for (int x = 0; x < results.size(); x++){
+			wordMatch[x] = wordSync(results.get(x));
+		}
+		int index = findGreatest(wordMatch);
+		System.out.println("\nWe believe that Shift " + (index + 1) + " is your best shot. It says '" + results.get(index) + "'.");
+	}
+	private static double wordSync(String message) throws FileNotFoundException{
+		ArrayList<String> words = messageBreak(message);
+		System.out.print(words.toString());
+		int real = 0;
+		for (int x = 0; x < words.size(); x++){
+			Scanner file = new Scanner(new File("C:/Files/dictionary.txt"));
+			boolean isReal = false;
+			while (file.hasNext()){
+				String test = file.next();
+				test = test.toLowerCase();
+				if (words.get(x).equals(test)){
+					isReal = true;
+					break;
+				}
+			}
+			if (isReal){
+				real++;
 			}
 		}
+		System.out.println(" " + real);
+		return ((double)real/words.size());
 	}
-	public static int bestGuess(ArrayList<String> results) throws FileNotFoundException{
-		Scanner input = new Scanner(new File("C:/files/Files/dictionary"));
-		int[] tally = new int[results.size()];
-		String message  = ""; 
-		for(int i = 0; i < results.size(); i++){
-			message = results.get(i); 
-			
+	private static ArrayList<String> messageBreak(String message){
+		ArrayList<String> addup = new ArrayList<String>();
+		String segment = "";
+		for (int x = 0; x < message.length(); x++){
+			if (message.charAt(x) == 32){
+				addup.add(segment);
+				segment = "";
+			} else {
+				segment += message.charAt(x);
+			}
 		}
+		addup.add(segment);
+		return addup;
+	}
+	private static int findGreatest(double[] array){
+		double highest = 0.0;
+		int index = 0;
+		for (int x = 0; x < array.length; x++){
+			if (array[x] >= highest){
+				index = x;
+				highest = array[x];
+			}
+		}
+		return index;
 	}
 }
